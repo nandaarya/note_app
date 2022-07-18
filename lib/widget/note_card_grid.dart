3a16b146bcc835b58cb7c40cc26731e1be8d6b77
Nanda Arya Putra <nandaarya404@gmail.com>
2022-07-18 13:35:10 +0700

    (+) add note widget

---
+++ lib/widget/note_card_grid.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class NoteCardGrid extends StatefulWidget {
+  const NoteCardGrid({Key? key}) : super(key: key);
+
+  @override
+  State<NoteCardGrid> createState() => _NoteCardGridState();
+}
+
+class _NoteCardGridState extends State<NoteCardGrid> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Card(
+        child: InkWell(
+          borderRadius: BorderRadius.circular(15.0),
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                      "judul",
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 20.0, color: Colors.black),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                        "isi catatan",
+                      maxLines: 6,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                          'waktu',
+                        style: TextStyle(color: Colors.black54, fontSize: 12.0),
+                      ),
+                      Text(
+                          'Tanggal',
+                        style: TextStyle(color: Colors.black54, fontSize: 12.0),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
