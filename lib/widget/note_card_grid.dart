0110e29e0502000a2898d08f7e218ff2affb2f65
Nanda Arya Putra <nandaarya404@gmail.com>
2022-07-18 21:16:43 +0700

    (+) add gridviewcount 2

--- lib/widget/note_card_grid.dart
+++ lib/widget/note_card_grid.dart
@@ -12,54 +12,57 @@ class _NoteCardGridState extends State<NoteCardGrid> {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 10),
-      child: Card(
-        child: InkWell(
-          borderRadius: BorderRadius.circular(15.0),
-          child: Container(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                      "judul",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 20.0, color: Colors.black),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: [Card(
+          child: InkWell(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
-                        "isi catatan",
-                      maxLines: 6,
-                      overflow: TextOverflow.fade,
-                      style: TextStyle(color: Colors.black54),
+                        "judul",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 20.0, color: Colors.black),
                     ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                          'waktu',
-                        style: TextStyle(color: Colors.black54, fontSize: 12.0),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                          "isi catatan",
+                        maxLines: 6,
+                        overflow: TextOverflow.fade,
+                        style: TextStyle(color: Colors.black54),
                       ),
-                      Text(
-                          'Tanggal',
-                        style: TextStyle(color: Colors.black54, fontSize: 12.0),
-                      )
-                    ],
+                    ),
                   ),
-                ),
-              ],
+                  Container(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                            'waktu',
+                          style: TextStyle(color: Colors.black54, fontSize: 12.0),
+                        ),
+                        Text(
+                            'Tanggal',
+                          style: TextStyle(color: Colors.black54, fontSize: 12.0),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
+        ),],
       ),
     );
   }
