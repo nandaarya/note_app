45502823e9ee79e116a0875fa7769958130a7e45
Nanda Arya Putra <nandaarya404@gmail.com>
2022-07-23 21:06:42 +0700

    (+) add ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:note_app/screen/main_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CatatanKu',
-      theme: ThemeData(),
-      home: MainScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        textTheme: const TextTheme(
+          headline5: TextStyle(
+              fontFamily: 'Sans',
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 24),
+          bodyText2: TextStyle(
+              fontFamily: 'Sans',
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 20),
+          bodyText1: TextStyle(
+              fontFamily: 'Sans',
+              fontWeight: FontWeight.normal,
+              color: Colors.black,
+              fontSize: 18),
+          subtitle2: TextStyle(
+              fontFamily: 'Sans',
+              fontWeight: FontWeight.normal,
+              color: Colors.black,
+              fontSize: 14),
+        ),
+      ),
+      home: const MainScreen(),
     );
   }
 }
