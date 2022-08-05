b16a65361947283f3585b78127aec66dc9994640
Nanda Arya Putra <nandaarya404@gmail.com>
2022-08-05 20:06:34 +0700

    (+) add splashscreen logo

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'main_screen.dart';
 import 'package:splashscreen/splashscreen.dart';
 
-class SplashScreenPage extends StatelessWidget {
-  const SplashScreenPage({Key key}) : super(key: key);
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +11,7 @@ class SplashScreenPage extends StatelessWidget {
       seconds: 5,
       navigateAfterSeconds: const MainScreen(),
       title: const Text('contoh splash screen'),
-      image: Image.asset('images/logobf.png'),
+      image: Image.asset('assets/images/splashscreen.png'),
       photoSize: 150.0,
     );
   }
