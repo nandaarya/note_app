192f2b642c3e5b9cbe30860437157f25790ca50c
Nanda Arya Putra <nandaarya404@gmail.com>
2022-08-07 21:21:22 +0700

    (+) change splash screen ui

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'main_screen.dart';
 import 'package:splashscreen/splashscreen.dart';
 
+/// Splash Screen class
 class SplashScreenPage extends StatelessWidget {
   const SplashScreenPage({Key key}) : super(key: key);
 
@@ -15,6 +16,7 @@ class SplashScreenPage extends StatelessWidget {
           'CatatanKu',
           style: TextStyle(color: Colors.white, fontSize: 40),
       ),
+      loadingText: const Text("memuat catatan..."),
       image: Image.asset('assets/images/splashscreen.png'),
       photoSize: 80.0,
     );
